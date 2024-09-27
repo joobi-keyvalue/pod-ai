c6ca2c0ff34c0e1046622e105c0a31b1be4267ad
rajat-durian <94341418+rajat-durian@users.noreply.github.com>
2024-09-27 14:46:31 +0530

    tts (#6)

--- backend/core/server/dependencies.go
+++ backend/core/server/dependencies.go
@@ -4,11 +4,13 @@ import (
 	"github.com/keycode/podai/auth"
 	"github.com/keycode/podai/db"
 	"github.com/keycode/podai/store"
+	"github.com/keycode/podai/tts"
 )
 
 type Dependencies struct {
 	UserStore   store.UserStorer
 	AuthService auth.Service
+	TTSService  tts.Service
 }
 
 func NewDependencies() (dependencies Dependencies, err error) {
@@ -16,9 +18,12 @@ func NewDependencies() (dependencies Dependencies, err error) {
 	userStore := store.NewUserStore(appDB)
 	authService := auth.NewService(userStore)
 
+	ttsService := tts.NewService(appDB)
+
 	dependencies = Dependencies{
 		UserStore:   userStore,
 		AuthService: authService,
+		TTSService:  ttsService,
 	}
 
 	return
