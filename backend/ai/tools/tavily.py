9f95b974304b5cbf1f08d8fc4a918544b1376d59
navnitms-kv <89079273+navnitms-kv@users.noreply.github.com>
2024-09-27 20:41:17 +0530

    Update AI Service (#13)

--- backend/ai/tools/tavily.py
+++ backend/ai/tools/tavily.py
@@ -1,7 +1,6 @@
 from tavily import TavilyClient
 import os
 from dotenv import load_dotenv
-from tools.openai import summarize_topic
 
 
 load_dotenv()
@@ -9,13 +8,16 @@ load_dotenv()
 tavily_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
 
 def fetch_content(topic, urls):
-        all_raw_content = ""
-        
-        response = tavily_client.extract(urls=urls)
+        if(topic == "Reddit"):
+                print("")
+        else:
+                all_raw_content = ""
 
-        for result in response["results"]:
-                print(f"URL: {result['url']}")
-                all_raw_content += f"URL: {result['url']}\n"
-                all_raw_content += f"{result['raw_content']}\n\n\n"
-        print(all_raw_content)
-        summarize_topic(topic, all_raw_content)
+                response = tavily_client.extract(urls=urls)
+
+                for result in response["results"]:
+                        print(f"URL: {result['url']}")
+                        all_raw_content += f"URL: {result['url']}\n"
+                        all_raw_content += f"{result['raw_content']}\n\n\n"
+                print(all_raw_content)
+                return all_raw_content
