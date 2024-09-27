f40ca22e3bf5c54c87cebfdc86519ffb11b139e6
gauthamsbaiju961 <145089418+gauthamsbaiju961@users.noreply.github.com>
2024-09-28 04:58:18 +0530

    fix add user topic (#21)
    Co-authored-by: Gautham-S-Baiju <gautham.baiju@durian.money>

--- backend/core/store/user_topic_query.go
+++ backend/core/store/user_topic_query.go
@@ -4,7 +4,7 @@ const (
 	addUserTopicInsertClause = `INSERT INTO user_topic
 	(user_id, topic_id) VALUES`
 
-	returningClause = `RETURNING user_id, topic_id;`
+	returningClause = `ON CONFLICT (user_id, topic_id) DO NOTHING RETURNING user_id, topic_id;`
 
 	getTopicByUserID = `SELECT t.id, t.name FROM 
              topic t INNER JOIN user_topic ut
