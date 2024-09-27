8833e149199a6a28ae12e86881819f6b6b0648c3
Gautham-S-Baiju <gautham.baiju@durian.money>
2024-09-27 11:34:00 +0530

    added get user endpoint

--- backend/core/server/http_router.go
+++ backend/core/server/http_router.go
@@ -10,7 +10,7 @@ func initRouter(dependencies Dependencies) (router *mux.Router) {
 	router = mux.NewRouter()
 	router.StrictSlash(true)
 
-	router.Handle("/user", auth.HandleGetUSer(dependencies.AuthService)).Methods(
+	router.Handle("/user/{id}", auth.HandleGetUserByID(dependencies.AuthService)).Methods(
 		http.MethodGet,
 	)
 	return
